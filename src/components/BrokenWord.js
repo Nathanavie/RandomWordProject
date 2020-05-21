47151721b0057b166f3d503a5482a21eef3f969d
Nathan Atherton <nathanatherton2@gmail.com>
2020-05-22 00:36:09 +0100

    Update BrokenWord.js
    Fixed spelling typo

--- src/components/BrokenWord.js
+++ src/components/BrokenWord.js
@@ -4,7 +4,7 @@ import Header from './Header';
 const BrokenWord = props => {
   return (
     <>
-      <Header wording="This is awkawrd..." />
+      <Header wording="This is awkward..." />
       <div className="content BrokenWord">
         <h2>It looks like Merriam Webster do not recognise "{props.randomWord}" and therefore they do not have a definition right now.</h2>
         <Button function={props.tryAgain} wording="Why not get a new word and try again?" />
